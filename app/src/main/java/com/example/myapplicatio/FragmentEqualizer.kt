1a53af250a728b3d752119a81c39866d7957c65d
Mathless <mrkalumba@gmail.com>
2019-06-20 14:46:11 +0300

    EQ saving

--- app/src/main/java/com/example/myapplicatio/FragmentEqualizer.kt
+++ app/src/main/java/com/example/myapplicatio/FragmentEqualizer.kt
@@ -1,5 +1,6 @@
 package com.example.myapplicatio
 
+import android.content.Context
 import android.media.MediaPlayer
 import android.media.audiofx.BassBoost
 import android.media.audiofx.Equalizer
@@ -14,14 +15,21 @@ import kotlinx.android.synthetic.main.fragment_fragment_equalizer.*
 import kotlinx.android.synthetic.main.fragment_fragment_main_activity.*
 import ru.spb.designedBy239School.advancedMusicPlayer.service.BackgroundAudioService
 
-class FragmentEqualizer :Fragment(){
+class FragmentEqualizer :Fragment() {
+
+    lateinit var eqSettings: Equalizer.Settings
+    lateinit var equalizer: Equalizer
+    var pauseCounter = 0
+
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_fragment_equalizer, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
-        var  audioSessionId =BackgroundAudioService::mediaPlayer.get(BackgroundAudioService()).audioSessionId
+        var audioSessionId = BackgroundAudioService::mediaPlayer.get(BackgroundAudioService()).audioSessionId
+
 
         //val bassboost = BassBoost(0, audioSessionId)
 
@@ -37,7 +45,7 @@ class FragmentEqualizer :Fragment(){
 //        })
 
 
-        val equalizer = Equalizer(1, audioSessionId)
+        this.equalizer = Equalizer(1, audioSessionId)
         equalizer.enabled = true
 
         if (equalizer.enabled && equalizer.hasControl()) Log.d(
@@ -51,9 +59,11 @@ class FragmentEqualizer :Fragment(){
         eq_seek_bar_0.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
             }
+
             override fun onStartTrackingTouch(p0: SeekBar?) {
                 equalizer.enabled = true
             }
+
             override fun onStopTrackingTouch(seekBar: SeekBar) {
                 equalizer.setBandLevel(0, seekBar.progress.toShort())
                 Log.d("EQ", "BAnd 0 level is ${equalizer.getBandLevel(0)}")
@@ -63,8 +73,10 @@ class FragmentEqualizer :Fragment(){
         eq_seek_bar_1.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
             }
+
             override fun onStartTrackingTouch(p0: SeekBar?) {
             }
+
             override fun onStopTrackingTouch(seekBar: SeekBar) {
                 equalizer.setBandLevel(1, seekBar.progress.toShort())
                 Log.d("EQ", "BAnd 1 level is ${equalizer.getBandLevel(0)}")
@@ -74,8 +86,10 @@ class FragmentEqualizer :Fragment(){
         eq_seek_bar_2.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
             }
+
             override fun onStartTrackingTouch(p0: SeekBar?) {
             }
+
             override fun onStopTrackingTouch(seekBar: SeekBar) {
                 equalizer.setBandLevel(2, seekBar.progress.toShort())
                 Log.d("EQ", "BAnd 2 level is ${equalizer.getBandLevel(2)}")
@@ -85,8 +99,10 @@ class FragmentEqualizer :Fragment(){
         eq_seek_bar_3.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
             }
+
             override fun onStartTrackingTouch(p0: SeekBar?) {
             }
+
             override fun onStopTrackingTouch(seekBar: SeekBar) {
                 equalizer.setBandLevel(3, seekBar.progress.toShort())
                 Log.d("EQ", "BAnd 3 level is ${equalizer.getBandLevel(3)}")
@@ -96,8 +112,10 @@ class FragmentEqualizer :Fragment(){
         eq_seek_bar_4.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
             }
+
             override fun onStartTrackingTouch(p0: SeekBar?) {
             }
+
             override fun onStopTrackingTouch(seekBar: SeekBar) {
                 equalizer.setBandLevel(4, seekBar.progress.toShort())
                 Log.d("EQ", "BAnd 4 level is ${equalizer.getBandLevel(4)}")
@@ -117,4 +135,45 @@ class FragmentEqualizer :Fragment(){
 
     }
 
+
+//    override fun onSaveInstanceState(outState: Bundle) {
+//        requireActivity().getSharedPreferences("Sas", Context.MODE_PRIVATE).edit().putInt("key", 1).apply()
+//        requireActivity().getSharedPreferences("SAs", Context.MODE_PRIVATE).getInt("key", 0)
+//    }
+
+    override fun onPause() {
+        super.onPause()
+        pauseCounter++
+        Log.d("EQ", "pauseCounter $pauseCounter")
+        requireActivity().getSharedPreferences("Eq_Settings", Context.MODE_PRIVATE)
+            .edit().putInt("band0", equalizer.properties.bandLevels[0].toInt()).apply()
+        requireActivity().getSharedPreferences("Eq_Settings", Context.MODE_PRIVATE)
+            .edit().putInt("band1", equalizer.properties.bandLevels[1].toInt()).apply()
+        requireActivity().getSharedPreferences("Eq_Settings", Context.MODE_PRIVATE)
+            .edit().putInt("band2", equalizer.properties.bandLevels[2].toInt()).apply()
+        requireActivity().getSharedPreferences("Eq_Settings", Context.MODE_PRIVATE)
+            .edit().putInt("band3", equalizer.properties.bandLevels[3].toInt()).apply()
+        requireActivity().getSharedPreferences("Eq_Settings", Context.MODE_PRIVATE)
+            .edit().putInt("band4", equalizer.properties.bandLevels[4].toInt()).apply()
+    }
+
+
+    //    !!!!!!!!!           ONRESUME CALLED
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("EQ", "ONRESUME called")
+        eq_seek_bar_0.progress =
+            requireActivity().getSharedPreferences("Eq_Settings", Context.MODE_PRIVATE).getInt("band0", 0)
+        eq_seek_bar_1.progress =
+            requireActivity().getSharedPreferences("Eq_Settings", Context.MODE_PRIVATE).getInt("band1", 0)
+        eq_seek_bar_2.progress =
+            requireActivity().getSharedPreferences("Eq_Settings", Context.MODE_PRIVATE).getInt("band2", 0)
+        eq_seek_bar_3.progress =
+            requireActivity().getSharedPreferences("Eq_Settings", Context.MODE_PRIVATE).getInt("band3", 0)
+        eq_seek_bar_4.progress =
+            requireActivity().getSharedPreferences("Eq_Settings", Context.MODE_PRIVATE).getInt("band4", 0)
+
+
+    }
 }
